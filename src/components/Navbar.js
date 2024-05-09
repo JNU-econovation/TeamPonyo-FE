36b1c4e9319994ad6ef235fd97caef76c5382a00
JeongSeoyun <xeonj@naver.com>
2024-05-09 17:11:13 +0900

    feat: NavBar 로그인 여부에 따라 헤더 요소들 visibility 조건 처리

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ import './Navbar.css'
 function Navbar() {
 
     const [userName, setUserName] = useState("서윤")
+    const [isLogin, setIsLogin] = useState(true)
     const [notification, setNotification] = useState(false)
+
 
     function isNotification(){
 
@@ -15,7 +17,9 @@ function Navbar() {
   return (
     <div className='Header'>
 
-        <div className='Hello'>반가워요, <span className='userName'>{userName}</span>님</div>
+        <div className={`Hello ${isLogin ? '' : 'hidden'}`}>
+            <p>반가워요, <span className='userName'>{userName}</span>님</p>
+        </div>
         <div className='HeaderContents'>
             <div className='NavLeft'></div>
             <div className='NavBar'>
@@ -29,9 +33,11 @@ function Navbar() {
                 </ul>
                 </div>
                 <div className='NavItem NavLogo'>OnJeon</div>
-                <div className='NavItem NavMenu'>MY PAGE</div>
+                <div className='NavItem NavMenu'>
+                    {isLogin ? 'MY PAGE' : 'LOGIN'}
+                </div>
             </div>
-            <div className='NavRight'>
+            <div className={`NavRight ${isLogin ? '' : 'hidden'}`}>
                 <span class="material-symbols-outlined">edit_square</span>
                 <span class="material-symbols-outlined">
                     {notification ? 'notifications_unread' : 'notifications'}
