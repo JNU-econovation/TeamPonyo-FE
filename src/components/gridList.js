99bf1accc674ff3aa945ab57925316056b50e21c
JeongSeoyun <xeonj@naver.com>
2024-05-23 17:22:59 +0900

    feat: GridList 호버 추가

--- src/components/gridList.js
+++ src/components/gridList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './GridList.css'
+import { ReactComponent as Arrow1 } from '../Arrow1.svg'
 
+//TODO: 추후 백엔드 연결시 아래 더미데이터 지우고, 데이터 받아오기
 const postList = [
   {
     title: "2021 홍익대 미대 전시회",
@@ -46,7 +48,16 @@ const GridList = () => {
       <div className='postGrid'>
       {postList.map((item, index) => (
         <div key={index}>
-          <img className='poster' src={item.path} alt='게시물'/>
+
+          <div className='imageContainer'>
+            <img className='poster' src={item.path} alt='게시물'/>
+            <div className='overlay'>
+              <div>자세히 보기</div>
+              <Arrow1 className='arrow'/>
+              <div className='moreEn'>more</div>
+            </div>
+          </div>
+        
           <div className='postInfo'>
             <div className='postTitle'>{item.title}</div>
             <div className='postPeriod'>{item.period}</div>
