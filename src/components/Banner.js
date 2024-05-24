9f0821a8667c5d5239a2e0a44d0d47629cfc651a
JeongSeoyun <xeonj@naver.com>
2024-05-24 21:33:55 +0900

    feat: 배너 추가

---
+++ src/components/Banner.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import './Banner.css';
+//import CardBanner from './CardBanner'
+
+const Banner = () => {
+
+  return (
+    <Swiper
+      direction={'horizontal'}
+      slidesPerView={5}
+      spaceBetween={300}
+      centeredSlides={true}
+      loop={true}
+      autoplay={{
+        delay: 5000,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+    >
+      <SwiperSlide>Slide1</SwiperSlide>
+      <SwiperSlide>Slide2</SwiperSlide>
+      <SwiperSlide>Slide3</SwiperSlide>
+      <SwiperSlide>Slide4</SwiperSlide>
+      <SwiperSlide>Slide5</SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Banner;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import GridList from '../components/GridList'
+import Banner from '../components/Banner'
+import CardBanner from '../components/CardBanner'
+
 
 const Main = () => {
   return (
     <div>
+      <CardBanner />
+      <Banner />
       <GridList />
     </div>
   )
