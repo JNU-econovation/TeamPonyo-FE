af17f19af9b831c036cf998009edb97d89f51e42
gyuminzzz <gminerva@kakao.com>
2024-05-28 23:05:39 +0900

    chore: MainSlide 배치 및 css 생성

---
+++ src/components/MainSlide.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './MainSlide.css'
+
+
+
+const CardBanner = () => {
+  return (
+    <div className='MainSlide'>
+        온전
+        
+    </div>
+  )
+}
+
+export default CardBanner
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,12 +2,13 @@ import React from 'react'
 import GridList from '../components/GridList'
 import Banner from '../components/Banner'
 import CardBanner from '../components/CardBanner'
-
+import MainSlide from '../components/MainSlide'
 
 const Main = () => {
   return (
     <div>
       <CardBanner />
+      <MainSlide />
       <Banner />
       <GridList />
     </div>
