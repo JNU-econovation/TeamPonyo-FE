203c280dc73c9c19da5b722816b5a719e595a590
gyuminzzz <gminerva@kakao.com>
2024-06-25 17:26:10 +0900

    feat: 메인배너 삭제

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,13 +2,12 @@ import React from 'react'
 import GridList from '../components/GridList'
 import Banner from '../components/Banner'
 import CardBanner from '../components/CardBanner'
-import MainSlide from '../components/MainSlide'
+// import MainSlide from '../components/MainSlide'
 import { SearchBox } from '../components/SearchBox'
 
 const Main = () => {
   return (
     <div>
-      <MainSlide />
       <SearchBox/>
       <CardBanner />
       <Banner />
