f367a3f9827701a75db8dbd5e6d6600f8d367345
JeongSeoyun <xeonj@naver.com>
2024-06-30 13:25:28 +0900

    feat: Banner 구현

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,32 +5,32 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import './Banner.css';
-//import CardBanner from './CardBanner.js';
+import CardBanner from './CardBanner.js';
 
 const Banner = () => {
 
   return (
     <Swiper
       direction={'horizontal'}
-      slidesPerView={5}
-      spaceBetween={300}
+      slidesPerView={3}
+      /** spaceBetween={200} **/
       centeredSlides={true}
       loop={true}
-      autoplay={{
-        delay: 5000,
-        disableOnInteraction: false,
-      }}
+      // autoplay={{
+      //   delay: 5000,
+      //   disableOnInteraction: false,
+      // }}
       pagination={{
         clickable: true,
       }}
       navigation={true}
       modules={[Autoplay, Pagination, Navigation]}
     >
-      <SwiperSlide>Slide1</SwiperSlide>
-      <SwiperSlide>Slide2</SwiperSlide>
-      <SwiperSlide>Slide3</SwiperSlide>
-      <SwiperSlide>Slide4</SwiperSlide>
-      <SwiperSlide>Slide5</SwiperSlide>
+      <SwiperSlide><CardBanner /></SwiperSlide>
+      <SwiperSlide><CardBanner /></SwiperSlide>
+      <SwiperSlide><CardBanner /></SwiperSlide>
+      <SwiperSlide><CardBanner /></SwiperSlide>
+      <SwiperSlide><CardBanner /></SwiperSlide>
     </Swiper>
   );
 };
