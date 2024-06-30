0d92fe5c95f81771181ad3f884840d7916951b92
JeongSeoyun <xeonj@naver.com>
2024-06-30 13:40:42 +0900

    style: CardBanner 여백 수정 및 position 수정

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import GridList from '../components/GridList'
 import Banner from '../components/Banner'
-import CardBanner from '../components/CardBanner'
 // import MainSlide from '../components/MainSlide'
 import { SearchBox } from '../components/SearchBox'
 
 const Main = () => {
   return (
-    <div>
+    <div className='Main'>
       <SearchBox/>
-      <CardBanner />
       <Banner />
       <GridList />
     </div>
