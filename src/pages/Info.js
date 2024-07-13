34160aa72b4e82c84b8805056bc79ea84bdc0476
JeongSeoyun <xeonj@naver.com>
2024-07-13 14:57:09 +0900

    feat: 전시 정보 페이지 구현

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,42 +1,51 @@
-import React from 'react'
+import React from 'react';
+import { infoData } from '../mokupData/infoData';
+import './Info.css'
 
 const Info = () => {
   return (
     <div className='Info'>
-        <div className='infoContainer'>
-            <div className='posterContainer'></div>
-            <div className='informationContainer'>
-                <div className='infoTag'></div>
-                <div className='infoTitleContainer'>
-                    <div className='infoTitle'></div>
-                </div>
-                <div className='infoAuthor'></div>
-                <div className='infoPlace'>장소 | {}</div>
-                <div className='infoPeriod'>기간 | </div>
-                <div className='infoTime'>시간 | </div>
-                <div className='infoPrice'>입장료 | </div>
-                <div className='infoAsk'>전시 문의 | </div>
-                <div className='btnContainer'>
-                    <button>저장</button>
-                    <button>관람 완료</button>
-                </div>
+      {infoData.map((data, index) => (
+        <div className='infoContainer' key={index}>
+          <div className='topContainer'>
+            <div className='posterContainer'>
+              <img src={data.poster} alt='포스터' />
             </div>
-        </div>
-        <div className='infoDescriptionContainer'>
+            <div className='informationContainer'>
+              <div className='infoTag'>{data.status}</div>
+              <div className='infoTitleContainer'>
+                <div className='infoTitle'>{data.title}</div>
+              </div>
+              <div className='infoAuthor'>{data.author}</div>
+              <div className='infoPlace'>장소 | {data.place}</div>
+              <div className='infoPeriod'>기간 | {data.period}</div>
+              <div className='infoTime'>시간 | {data.time}</div>
+              <div className='infoPrice'>입장료 | {data.price}</div>
+              <div className='infoAsk'>전시 문의 | {data.ask}</div>
+              <div className='btnContainer'>
+                <button>저장</button>
+                <button>관람 완료</button>
+              </div>
+            </div>
+          </div>
+          <div className='infoDescriptionContainer'>
             <div className='descriptionTitle'>소개</div>
-            <div className='descriptionBody'></div>
-        </div>
-        <div className='infoPhotoContainer'>
-
-        </div>
-        <div className='infoPlaceContainer'>
+            <div className='descriptionBody'>{data.description}</div>
+          </div>
+          <div className='infoPhotoContainer'>
+            {/* Add the image if needed */}
+          </div>
+          <div className='infoPlaceContainer'>
             <div className='infoPlaceTitle'>공간 정보</div>
-            <div className='infoPlaceBody'></div>
-            <div className='infoPlaceMap'></div>
+            <div className='infoPlaceBody'>{data.place}</div>
+            <div className='infoPlaceMap'>
+              {/* You can add a map component here */}
+            </div>
+          </div>
         </div>
-
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Info
+export default Info;
