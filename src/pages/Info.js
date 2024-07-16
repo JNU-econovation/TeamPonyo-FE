c125c49f8f49f75aa0f994811e4815a53de89524
JeongSeoyun <xeonj@naver.com>
2024-07-17 03:38:11 +0900

    feat: 전시 작성페이지 버튼 구현 및 전시 정보페이지 레이아웃 수정

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { infoData } from '../mokupData/infoData';
 import './Info.css'
 
 const Info = () => {
+
+    const [isSave, setIsSave] = useState(false)
+    const [isCompleted, setIsCompleted] = useState(false)
+
   return (
     <div className='Info'>
       {infoData.map((data, index) => (
@@ -14,17 +18,25 @@ const Info = () => {
             <div className='informationContainer'>
               <div className='infoTag'>{data.status}</div>
               <div className='infoTitleContainer'>
-                <div className='infoTitle'>{data.title}</div>
+                <div className='infoTitle infoItem'><div className='infoTitleText'>{data.title}</div></div>
               </div>
               <div className='infoAuthor'>{data.author}</div>
-              <div className='infoPlace'>장소 | {data.place}</div>
-              <div className='infoPeriod'>기간 | {data.period}</div>
-              <div className='infoTime'>시간 | {data.time}</div>
-              <div className='infoPrice'>입장료 | {data.price}</div>
-              <div className='infoAsk'>전시 문의 | {data.ask}</div>
-              <div className='btnContainer'>
-                <button>저장</button>
-                <button>관람 완료</button>
+              <div className='infoPlace infoItem'>장소  |  {data.place}</div> 
+              <div className='infoPeriod infoItem'>기간  |  {data.period}</div>
+              <div className='infoTime infoItem'>시간  |  {data.time}</div>
+              <div className='infoPrice infoItem'>입장료  |  {data.price}</div>
+              <div className='infoAsk infoItem'>전시 문의  |  {data.ask}</div>
+              <div className='infoBtnContainer'>
+                <div
+                    className={`middleBtnGray ${isSave ? 'active' : ''}`}
+                    onClick={() => setIsSave(!isSave)}>
+                    저장
+                </div>
+                <div
+                    className={`middleBtnGray ${isCompleted ? 'active' : ''}`}
+                    onClick={() => setIsCompleted(!isCompleted)}>
+                    관람완료
+                </div>
               </div>
             </div>
           </div>
@@ -33,13 +45,13 @@ const Info = () => {
             <div className='descriptionBody'>{data.description}</div>
           </div>
           <div className='infoPhotoContainer'>
-            {/* Add the image if needed */}
+            {/* 첨부된 사진들 불러올 곳 */}
           </div>
           <div className='infoPlaceContainer'>
             <div className='infoPlaceTitle'>공간 정보</div>
             <div className='infoPlaceBody'>{data.place}</div>
             <div className='infoPlaceMap'>
-              {/* You can add a map component here */}
+              {/* 공간정보 카카오맵 */}
             </div>
           </div>
         </div>
