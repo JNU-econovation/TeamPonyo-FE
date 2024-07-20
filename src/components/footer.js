c0b499228f06d4f3f276e8045d56a60aeacf51a4
JeongSeoyun <xeonj@naver.com>
2024-07-20 13:38:54 +0900

    feat: 팀계정 로그인 sessionStorage 테스트 통신 코드 추가

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './Footer.css'
+import axiosInstance from '../api/axiosInstance'
 
 const footer = () => {
+
+  const handleLogoClick = async () => {
+    try {
+      const response = await axiosInstance.post('/api/v1/auth/signup/team', {
+        access_token: "sample"
+      })
+      const accessToken = response.data.access_token
+      console.log('team test accessToken: ', accessToken)
+      sessionStorage.setItem('access_token', accessToken)
+    } catch (error) {
+      console.error ('테스트 팀계정 로그인 에러', error)
+    }
+  }
+
   return (
     <div className='Footer'>
-        <div>서비스 로고</div>
-        <div className='FooterBottom'>이메일 문의 / 개인정보 처리방침</div>
+        <div className='footerLogo' onClick={handleLogoClick}>서비스 로고</div>
+        <div className='footerBottom'>이메일 문의 / 개인정보 처리방침</div>
     </div>
   )
 }
