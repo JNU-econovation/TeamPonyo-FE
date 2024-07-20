d42293700d7cf0b89109cbcb0dbaa71bb6c2de88
JeongSeoyun <xeonj@naver.com>
2024-07-20 16:57:24 +0900

    feat: 전시 작성페이지 사진 형식 및 용량 제한 추가

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:8080',
-    timeout: 1000,
+    timeout: 3000,
     headers: {'Content-Type': 'application/json'}
 })
 
--- src/components/exhibition/UploadFile.js
+++ src/components/exhibition/UploadFile.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './UploadFile.css';
 
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 파일 크기 5MB 제한
+
 const UploadFile = ({ selectedFiles, onFilesSelect }) => {
   const handleFileChange = (e) => {
     const files = Array.from(e.target.files);
-    onFilesSelect(files);
+
+    const validFiles = files.filter(file => {
+        if (file.size > MAX_FILE_SIZE) {
+          alert(`${file.name} 파일이 너무 큽니다. 5MB 이하의 파일만 업로드해주세요.`);
+          return false;
+        }
+        // if (!file.type.startsWith('image/')) {
+        //   alert(`${file.name} 파일은 이미지 파일이 아닙니다. 이미지 파일만 업로드해주세요.`);
+        //   return false;
+        // }
+        return true;
+      });
+
+    onFilesSelect(validFiles);
   };
 
   return (
     <div className='UploadFile'>
-      <input type="file" id="fileUpload" multiple onChange={handleFileChange} className='fileInput' />
+      <input type="file" id="fileUpload" multiple onChange={handleFileChange} className='fileInput' accept='image/*' />
       <label htmlFor="fileUpload" className="middleBtn">파일</label>
       <div className="filePreview">
         {selectedFiles.map((file, index) => (
