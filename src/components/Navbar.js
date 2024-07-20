54e7d55fab780622104d446447b2d05768294c43
JeongSeoyun <xeonj@naver.com>
2024-07-20 17:21:45 +0900

    feat: 네비바 작성 아이콘 클릭시 작성페이지로 이동

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,7 +36,7 @@ function Navbar() {
                     <p>반가워요, <span className='nickName'><Link to={'/mypage'}>{nickName}</Link></span>님</p>
                 </div>
                 <div className='NavIcon'>
-                    <span class="material-symbols-outlined" ><Link to={'/edit'}>edit_square</Link></span>
+                    <span class="material-symbols-outlined" ><Link to={'/create'}>edit_square</Link></span>
                     <span class="material-symbols-outlined">
                         {notification ? 'notifications_unread' : 'notifications'}
                     </span> 
