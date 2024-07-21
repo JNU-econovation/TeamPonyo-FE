cc42d383de21813a30e8f95938af42aea667d8c9
gyuminzzz <gminerva@kakao.com>
2024-07-22 03:15:34 +0900

    chore: 에코노베이션 프로필 설정

--- src/components/MyGridList.js
+++ src/components/MyGridList.js
@@ -1,45 +1,53 @@
-import React from 'react';
-import './GridList.css';
-import { ReactComponent as Arrow1 } from '../Arrow1.svg';
+import React, { useEffect, useState } from 'react';
+import './GridList.css'
+import { ReactComponent as Arrow1 } from '../Arrow1.svg'
+import { Link } from 'react-router-dom';
 
+//TODO: 추후 백엔드 연결시 아래 더미데이터 지우고, 데이터 받아오기
+
+
+//TODO: 하단 girdList -> gridList 오타 수정
 const MyGridList = ({ data }) => {
-  const getTag = (status) => {
-    if (status === "ONGOING") {
-      return '전시 중';
-    } else if (status === "BEFORE") {
-      return '진행 예정';
-    } else if (status === "AFTER") {
-      return '진행 완료';
-    }
-    return '';
-  };
 
+    const getTag = (status) => {
+        if (status === "ONGOING") {
+          return '전시 중';
+        } else if (status === "BEFORE") {
+          return '진행 예정';
+        } else if (status === "AFTER") {
+          return '진행 완료';
+        }
+        return '';
+      };
+
+    
   return (
     <div className='gridList'>
       <div className='postGrid'>
-        {data.map((item, index) => (
-          <div key={index} className='gridItem'>
-            <div className='imageContainer'>
-              <img 
-                src={`data:${item.poster.mime_type};base64,${item.poster.base64_image}`} 
-                alt='포스터' 
-                className='poster' 
-              />
-              <div className='overlay'>
-                <div>자세히 보기</div>
-                <Arrow1 className='arrow'/>
-                <div className='moreEn'>more</div>
-              </div>
-            </div>
-            <div className='postInfo'>
-              <div className='postTitle'>{item.title}</div>
-              <div className='postPeriod'>{item.period}</div>
-              <div className='postState'>{getTag(item.exhibit_status)}</div>
-            </div>
-          </div>
+        {/* 임의 데이터 */}
+        {data && data.map((item, index) => (
+          <div key={index}>
+            <Link to={`/info/${item.exhibit_id}`}>
+                <div className='imageContainer'>
+                <img src={item.poster.base64_image} alt='포스터' />
+                {/* <img className='poster' src={item.path} alt='게시물'/> */}
+                <div className='overlay'>
+                    <div>자세히 보기</div>
+                    <Arrow1 className='arrow'/>
+                    <div className='moreEn'>more</div>
+                </div>
+                </div>
+                <div className='postInfo'>
+                <div className='postTitle'>{item.title}</div>
+                <div className='postPeriod'>{item.period}</div>
+                <div className='postState'>{getTag(item.exhibit_status)}</div>
+                </div>
+            </Link>
+        </div>
         ))}
       </div>
     </div>
+    
   );
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,43 +1,27 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './Profile.css'; 
 
 const Profile = ({ userId }) => {
-  const [profileData, setProfileData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const mockProfileData = {
+    profileImage: process.env.PUBLIC_URL + '/econoLogo.png',
+    nickname: "ECONOVATION",
+    Id: "econovation",
+    introduction: "전남대학교 IT 개발 동아리 에코노베이션입니다."
+  };
 
-  useEffect(() => {
-    const fetchProfile = async () => {
-      try {
-        const response = await axios.get(`/${userId}/profile`);
-        setProfileData(response.data);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProfile();
-  }, [userId]);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
-
-  const defaultImage = process.env.PUBLIC_URL + '/defaultProfile.png';
+  const { profileImage, nickname, Id, introduction } = mockProfileData;
 
   return (
     <div className="profile">
       <img 
-        src={profileData.profileImage || defaultImage} 
+        src={profileImage} 
         alt="Profile" 
         className="profile-image"
       />
       <div className="profile-details">
-        <h2 className="nickname">{profileData.nickname}</h2>
-        <p className="id">@{profileData.Id}</p>
-        <p className="introduction">{profileData.introduction}</p>
+        <h2 className="nickname">{nickname}</h2>
+        <p className="id">@{Id}</p>
+        <p className="introduction">{introduction}</p>
       </div>
     </div>
   );
