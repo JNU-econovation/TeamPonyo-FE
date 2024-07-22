5afa1653ea92ce996bba697a68c53657d7f09d6e
JeongSeoyun <xeonj@naver.com>
2024-07-22 13:43:48 +0900

    feat: 메인페이지 그리드 리스트 및 전시 정보페이지 이미지 구현

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -104,7 +104,7 @@ const Info = () => {
             <div className='infoContainer'>
                 <div className='topContainer'>
                     <div className='posterContainer'>
-                        <img src={data.poster.base64_image} alt='포스터' />
+                        <img src={data.poster_url} alt='포스터' />
                     </div>
                     <div className='informationContainer'>
                         <div className='infoTag'>{tag}</div>
@@ -141,6 +141,11 @@ const Info = () => {
                 </div>
                 <div className='infoPhotoContainer'>
                     {/* 첨부된 사진들 불러올 곳 */}
+                    {data.photo_urls.map((item, index) => (
+                        <div key={index}>
+                            <img src={item} alt={`첨부 사진 ${index + 1}`} />
+                        </div>
+                    ))}
                 </div>
                 <div className='infoMapContainer'>
                     <div className='infoPlaceTitle'>공간 정보</div>
