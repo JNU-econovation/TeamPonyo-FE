9ee903bfc50bd7b8068301bd0a3e4ed04c45fb8a
JeongSeoyun <xeonj@naver.com>
2024-07-23 19:28:34 +0900

    style: 카드배너 스타일 수정

--- src/components/cardBanner.js
+++ src/components/cardBanner.js
@@ -2,12 +2,21 @@ import React from 'react';
 import './CardBanner.css';
 
 const CardBanner = ({ item }) => {
+
+  const isAllEnglish = (text) => {
+    // 문자열이 모두 영어 알파벳으로만 구성되어 있는지 검사하는 정규식
+    return /^[A-Za-z\s]+$/.test(text);
+  };
+
+
   return (
     <div className='CardBanner'>
       <div className='CardContainer'>
-        <img className='CardPoster' src={item.poster} alt="Poster" />
+        <div className='CardPosterContainer'>
+          <img className='CardPoster' src={item.poster} alt="Poster" />
+        </div>
         <div className='CardInfo'>
-          <div className='CardTitle CardRight'>{item.title}</div>
+          <div className={`CardTitle CardRight ${isAllEnglish(item.title) ? 'Eng' : 'noEng'}`}>{item.title}</div>
           <div className='CardAuthor CardRight'>{item.author}</div>
           <div className='CardContent CardRight'>{item.content}</div>
           <div className='CardDate CardRight'>{item.date}</div>
