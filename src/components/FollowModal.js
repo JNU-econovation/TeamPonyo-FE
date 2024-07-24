e25bfaa0031da427747d20afe64c4cbae8519c4d
gyuminzzz <gminerva@kakao.com>
2024-07-25 02:02:47 +0900

    design: 팔로우 및 팔로우 목록 컴포넌트 수정

--- src/components/FollowModal.js
+++ src/components/FollowModal.js
@@ -1,14 +1,40 @@
-import React from 'react';
-//import './FollowModal.css'; // FollowModal에 대한 스타일링
+import React, { useState, useEffect } from 'react';
+import './FollowModal.css'; // FollowModal에 대한 스타일링
 
-const FollowModal = ({ title, content, onClose }) => {
+const FollowModal = ({ initialTab, followers, followings, onClose }) => {
+    const [activeTab, setActiveTab] = useState(initialTab);
+
+    useEffect(() => {
+        setActiveTab(initialTab);
+    }, [initialTab]);
+
+    const handleTabClick = (tab) => {
+        setActiveTab(tab);
+    };
+
+    const content = activeTab === 'followers' ? followers : followings;
+
     return (
         <div className="modal-overlay" onClick={onClose}>
             <div className="modal" onClick={(e) => e.stopPropagation()}>
-                <h2>{title}</h2>
-                <ul>
+                <div className="modal-header">
+                    <button 
+                        className={`tab-button ${activeTab === 'followers' ? 'active' : ''}`} 
+                        onClick={() => handleTabClick('followers')}
+                    >
+                        팔로워
+                    </button>
+                    <button 
+                        className={`tab-button ${activeTab === 'followings' ? 'active' : ''}`} 
+                        onClick={() => handleTabClick('followings')}
+                    >
+                        팔로잉
+                    </button>
+                    <button className="modal-close-button" onClick={onClose}>X</button>
+                </div>
+                <ul className="modal-content">
                     {content.map((user) => (
-                        <li key={user.user_id}>
+                        <li key={user.user_id} className="modal-item">
                             <img src={user.profile_image_url} alt={`${user.nickname}'s profile`} className="modal-profile-image" />
                             <div className="modal-profile-details">
                                 <span className="modal-nickname">{user.nickname}</span>
@@ -18,7 +44,6 @@ const FollowModal = ({ title, content, onClose }) => {
                         </li>
                     ))}
                 </ul>
-                <button className="modal-close-button" onClick={onClose}>닫기</button>
             </div>
         </div>
     );
